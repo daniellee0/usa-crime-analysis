f3acb6058cdc69e8c48fc7436dd7e0cf654ba679
Tammy Ho <tammyh0@uw.edu>
2018-03-07 13:39:12 -0800

    Finish county crimes for server

--- server.R
+++ server.R
@@ -28,12 +28,16 @@ server <- function(input, output) {
   ##############
   ### Part 2 ###
   ##############
+  
+  # Loads helper function that gets county name from hover coordinates
   source("counties-helper.R")
   
+  # Loads all county crime data from 2004 to 2016
   all.counties.years <- 
     read.csv("data/violent_county_crimes_all_years.csv", 
              stringsAsFactors = FALSE)
   
+  # Returns the chosen crime type
   chosen.crime <- reactive({
     col.name <- input$select.crime
     col.name.sym <- rlang::sym(col.name)
@@ -41,6 +45,8 @@ server <- function(input, output) {
     return(col.name.sym)
   })
   
+  # Returns data frame of counties from a chosen state during a chosen year
+  # and their crime reporting count for the chosen crime
   data.chosen.year <- reactive({
     chosen.year.data <- all.counties.years %>%
       filter(State == input$select.state) %>%
@@ -52,17 +58,8 @@ server <- function(input, output) {
     return(chosen.year.data)
   })
   
-  #data.2016 <- reactive({
-  #  data.2016 <- all.counties.years %>%
-  #    filter(State == input$slide.state) %>%
-  #    filter(Year == 2016) %>%
-  #    select(County, !!chosen.crime())
-  #  colnames(data.2016) <- c("subregion", "Chosen_Crime_2016")
-  #  data.2016$subregion <- tolower(data.2016$subregion)
-    
-  #  return(data.2016)
-  #})
-  
+  # Returns a map data frame of the chosen state with its crime rates for
+  # the selected year and chosen crime
   county.map <- reactive({
     counties <- map_data("county")
     chosen.state.counties <- subset(counties, 
@@ -73,18 +70,8 @@ server <- function(input, output) {
     return(county.map)
   })
   
-  output$county.plot <- renderPlot({
-    map <- ggplot(data = county.map()) +
-      geom_polygon(aes(x = long, y = lat, group = group, 
-                       fill = Chosen_Year_Crime), 
-                   color = "white", size = 0.6) +
-      scale_fill_gradient(low = "#fef0d9", high = "#b30000", 
-                          name = "# of crime reports")
-    
-    return(map)
-  })
-  
-  output$county.plot.info <- renderText({
+  # Returns the name of the county that was hovered over
+  hovered.county <- reactive({
     chosen.longitude <- input$county.plot.hover$x
     chosen.latitude <- input$county.plot.hover$y
     if ((is.null(chosen.longitude)) && (is.null(chosen.latitude))) {
@@ -95,14 +82,46 @@ server <- function(input, output) {
     chosen.county <- latlong2county(hover.points)
     chosen.county <- unlist(strsplit(chosen.county, ","))[2]
     
+    return(chosen.county)
+  })
+  
+  # Returns the number of reportings for the chosen crime 
+  # and the county that was hovered over
+  chosen.crime.count <- reactive({
     crime.report.count <- data.chosen.year() %>%
-      filter(subregion == chosen.county) 
+      filter(subregion == hovered.county()) 
     crime.report.count <- crime.report.count[1, 2]
     
-    statement <- paste0("County: ", toupper(chosen.county), "\n", 
-                        "Number of Reports: ", crime.report.count)
+    return(crime.report.count)
+  })
+  
+  # Returns a color-coded map of the counties within a chosen state
+  # based on their crime reporting count
+  output$county.plot <- renderPlot({
+    map <- ggplot(data = county.map()) +
+      geom_polygon(aes(x = long, y = lat, group = group, 
+                       fill = Chosen_Year_Crime), 
+                   color = "white", size = 0.6) +
+      scale_fill_gradient(low = "#fef0d9", high = "#b30000", 
+                          name = "# of reports")
+    
+    return(map)
+  })
+  
+  # Returns a statement about the county that was hovered over
+  output$county.plot.info <- renderText({
+    statement <- paste0(toupper(hovered.county()), " COUNTY", "\n", 
+                        "Number of Reports: ", chosen.crime.count())
       
     return(statement)
+  })
+  
+  output$map.title <- renderText({
+    chosen.crime <- gsub("\\.", " ", input$select.crime)
+    title <- paste0(input$slide.year, " ", chosen.crime, 
+                    " Reports in ", input$select.state)
+    
+    return(title)
   })
   
   ##############
