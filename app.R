6b25701261a38a03e4a46b9e74f1b9f5b73333f0
Lea Quan <myhenry8998@gmail.com>
2018-03-08 00:59:59 -0800

    Add completed tab 4

--- app.R
+++
@@ -1,7 +0,0 @@
-library("shiny") 
-
-source("ui.R")
-source("server.R")
-
-# create and run the shiny app
-shinyApp(ui = ui, server = server)
--- ui.R
+++ ui.R
@@ -39,9 +39,19 @@ ui <- fluidPage(
              ##############
              ### Part 4 ###
              ##############
-             tabPanel("Tab 4",
+             tabPanel("Hate Crimes By Category",
                 # Include a title
                 titlePanel("Hate Crime Statistics"),
+                
+                p(strong("Introduction: "), "This section compares the
+                  number of incidents for hate crimes against 
+                  different categories from 2005 to 2016. The question we
+                  hope to answer is how do the trends we see over time
+                  correspond to real world events? These trends should give 
+                  individuals a better sense of the tensions that exist 
+                  toward different groups. They should aslo help law enforcement initiate 
+                  preventative programs to defuse these tensions.", 
+                  align = "left"),
                 
                 # Include a `sidebarLayout()`
                 sidebarLayout(
@@ -51,7 +61,7 @@ ui <- fluidPage(
                     # A slectInput labeled "Bias Motivation", with choices 
                     # "Race", "Religion", "Sexual Orientaiton" and "Disability"
                     selectInput(inputId = "category",
-                                label = "Bias Motivation:",
+                                label = "Category:",
                                 choices = c("Race", "Religion", 
                                             "Sexual Orientation", 
                                             "Disability"),
@@ -63,7 +73,38 @@ ui <- fluidPage(
                   mainPanel(
                     
                     # A line graph showing the trend of hate crime incidents
-                    plotOutput('hatecrime')
+                    plotOutput('hatecrime'),
+                    p(strong("Race: "), "The graph shows a steady trend
+                      for most of the ethnic groups. However, there is 
+                      a dramatic drop for hate crimes against African
+                      Americans starting from 2008, which is when president 
+                      Obama was elected, until 2016.", 
+                      align = "left"),
+                    p(strong("Religion: "), "The graph shows that anti-islamic
+                      incidents increased dramaticly after 2014, which is when 
+                      the Islamic State seized large swathes of territory in
+                      Syria and Iraq. On the other hand, anti-Jewish incidents
+                      had dropped since 2008.", 
+                      align = "left"),
+                    p(strong("Sexual Orientation: "), "The graph shows 
+                      that the number of anti-homosexual incidents dropped steadily
+                      since 2011 when New York became the largest state to allow
+                      same-sex marriage.", align = "left"),
+                    p(strong("Disability: "), "The graph shows 
+                      a huge drop for anti-mental disability incidents in 2010.
+                      The year in which The Equality Act was passed
+                       to protect people with disabilities and prevent disability 
+                      discrimination. However, the number bounced back up from
+                      2010 to 2012.", align = "left"),
+                    br(),
+                    # Writes a disclaimer message
+                    em(p(strong("Disclaimer: "), "The categories above were
+                            provided by the FBI publications. It does not
+                            include all the hate crimes against each group.
+                            Additionally, the visualization above does not 
+                            account for the population size for each group
+                            which may slightly alter the meaning of the crime 
+                            report."))
                   )
                 )
              )     
@@ -71,6 +112,8 @@ ui <- fluidPage(
                       
                       
              ),
+             br(),
+             hr(),
              p("INFO 201 | Winter 2018 | Lea Quan, Tammy Ho, Ciarra Hart, Daniel Lee", align = "center")
   )
   
